441b517ffccbca056abdb82a38f067fce4e0203d
davidmarsden <dmarsd@gmail.com>
2025-09-03 13:52:17 +0100

    Update sheets.ts

--- src/server/sheets.ts
+++ src/server/sheets.ts
@@ -1,10 +1,11 @@
 import { google } from 'googleapis';
 
 export async function appendRows(sheetName: string, rows: (string|number|null)[][]) {
+  const key = (process.env.GOOGLE_SA_PRIVATE_KEY || '').replace(/\\n/g, '\n');
   const auth = new google.auth.JWT(
     process.env.GOOGLE_SA_EMAIL,
     undefined,
-    (process.env.GOOGLE_SA_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+    key || undefined,
     ['https://www.googleapis.com/auth/spreadsheets']
   );
   const sheets = google.sheets({ version: 'v4', auth });
