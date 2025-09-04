24782b7462548555c9979058c565512071953915
davidmarsden <dmarsd@gmail.com>
2025-09-04 09:37:03 +0100

    Create SeasonContext.tsx

---
+++ src/components/SeasonContext.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React, { createContext, useContext, useMemo } from 'react';
+
+type SeasonContextValue = {
+  season: string;
+};
+
+const SeasonContext = createContext<SeasonContextValue | undefined>(undefined);
+
+export function SeasonProvider({
+  season,
+  children,
+}: {
+  season: string;
+  children: React.ReactNode;
+}) {
+  const value = useMemo(() => ({ season }), [season]);
+  return <SeasonContext.Provider value={value}>{children}</SeasonContext.Provider>;
+}
+
+export function useSeason() {
+  const ctx = useContext(SeasonContext);
+  if (!ctx) {
+    throw new Error('useSeason must be used within a SeasonProvider');
+  }
+  return ctx;
+}
+
+export default SeasonContext;
